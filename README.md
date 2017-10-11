fd037d59935fb562e74f2d8bb9bcf331adb2b43a
yanek1988 <mis@uszatek.com>
2017-10-11 18:18:12 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Raspberry Car
+
+Library for car's computer for the following functions:
+* Store car's data from OBD II
+* (NOT SUPPORTED YET) Store GPS locations
+* (NOT SUPPORTED YET) Show GPS Navigartion on the screen
+
+Required components:
+* Raspberry Pi Model B Revision 2.0
+* OBD II ELM 327 BLUETOOTH
+
+Software:
+* Raspbian
+* Python 3
+
---
+++ setup/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt-get purge --auto-remove scratch debian-reference-en dillo idle3 python3-tk idle python-pygame python-tk lightdm \
+	gnome-themes-standard gnome-icon-theme raspberrypi-artwork gvfs-backends gvfs-fuse desktop-base lxpolkit netsurf-gtk \
+	zenity xdg-utils mupdf gtk2-engines alsa-utils  lxde lxtask menu-xdg gksu midori xserver-xorg xinit xserver-xorg-video-fbdev \
+	libraspberrypi-dev libraspberrypi-doc dbus-x11 libx11-6 libx11-data libx11-xcb1 x11-common x11-utils lxde-icon-theme \
+	gconf-service gconf2-common
+apt-get --yes autoremove
+apt-get --yes autoclean
+apt-get --yes clean
+
+apt-get install bluetooth bluez-utils blueman bluez python-gobject python-gobject-2
+
+pip3 install obd
